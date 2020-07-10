fc6097c696ff682040a1cd50f70b9c1083ca5476
beya765 <kenshinpower2000@yahoo.co.jp>
2020-07-10 00:50:55 +0000

    ユーザー削除機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user,  only: [:edit, :update]
-  before_action :correct_user,    only: [:edit, :update]
+  before_action :logged_in_user,  only: [:edit, :update, :destroy]
+  before_action :correct_user,    only: [:edit, :update, :destroy]
 
   def new
     @user = User.new
@@ -27,17 +27,21 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params) && !@user.saved_change_to_image?
       flash[:success] = "プロフィールの更新が完了しました"
       redirect_to @user
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @user.destroy 
+    flash[:success] = "ユーザーのデータを削除しました"
+    redirect_to root_url
   end
 
   private
